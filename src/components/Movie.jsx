624d42fc274bb6ad84f463b5bf8eb9074d35f798
Seogun95 <ksj083895@gmail.com>
2023-02-17 09:30:40 +0900

    feat: useParam을 통해 Detail 페이지에 전달받은 id를 받아옴

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 function Movie({ ...props }) {
   return (
     <>
       <div>
         <img src={props.img} alt={'이미지'} />
-        <h3>{props.title}</h3>
+        <h1>
+          <Link to={`/movie/${props.id}`}>{props.title}</Link>
+        </h1>
         <p>{props.summary}</p>
         <div>
           {props.genres?.map((v) => (
@@ -19,6 +22,7 @@ function Movie({ ...props }) {
 }
 
 Movie.propTypes = {
+  id: PropTypes.number.isRequired,
   img: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 function Detail() {
-  return <div>Detail</div>;
+  //동적으로 변경되는 url의 ID를 받아옴
+  const { id } = useParams();
+  console.log(id);
+
+  useEffect(() => {
+    const getMovies = async () => {
+      const json = await (
+        await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+      ).json();
+      console.log(json);
+    };
+
+    getMovies();
+  }, [id]);
+
+  return <></>;
 }
 
 export default Detail;
